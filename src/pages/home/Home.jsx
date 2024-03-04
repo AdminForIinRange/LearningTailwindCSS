228a99deaf7b3d5709a1ca29b42f9c10b24ff0b3
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-04 14:59:17 +1030

    Initial commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+  <div className="w-screen flex justify-center">
+    <h1 className="text-5xl md:text-2xl lg:text-6xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+  )
+}
